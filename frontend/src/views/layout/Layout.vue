ec86b20cfb1f998b0da7a8283ad9c46bd0ccd1d9
ZeuC <719034075@qq.com>
2018-08-02 17:03:06 +0800

    login complete

--- frontend/src/views/layout/Layout.vue
+++ frontend/src/views/layout/Layout.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="app-wrapper" :class="{hideSidebar:!sidebar.opened}">
+  <div class="app-wrapper" :class="classObj">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"></div>
     <sidebar class="sidebar-container"></sidebar>
     <div class="main-container">
       <navbar></navbar>
@@ -9,29 +10,60 @@
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from '@/views/layout/components'
+    import { Navbar, Sidebar, AppMain } from './components'
+    import ResizeMixin from './mixin/ResizeHandler'
 
-export default {
-  name: 'layout',
-  components: {
-    Navbar,
-    Sidebar,
-    AppMain
-  },
-  computed: {
-    sidebar() {
-      return this.$store.state.app.sidebar
+    export default {
+        name: 'layout',
+        components: {
+            Navbar,
+            Sidebar,
+            AppMain
+        },
+        mixins: [ResizeMixin],
+        computed: {
+            sidebar() {
+                return this.$store.state.app.sidebar
+            },
+            device() {
+                return this.$store.state.app.device
+            },
+            classObj() {
+                return {
+                    hideSidebar: !this.sidebar.opened,
+                    openSidebar: this.sidebar.opened,
+                    withoutAnimation: this.sidebar.withoutAnimation,
+                    mobile: this.device === 'mobile'
+                }
+            }
+        },
+        methods: {
+            handleClickOutside() {
+                this.$store.dispatch('CloseSideBar', { withoutAnimation: false })
+            }
+        }
     }
-  }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-@import "src/styles/mixin.scss";
-.app-wrapper {
-  @include clearfix;
-  position: relative;
-  height: 100%;
-  width: 100%;
-}
+  @import "src/styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    &.mobile.openSidebar{
+      position: fixed;
+      top: 0;
+    }
+  }
+  .drawer-bg {
+    background: #000;
+    opacity: 0.3;
+    width: 100%;
+    top: 0;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+  }
 </style>
--- frontend/src/views/layout/components/Sidebar/index.vue
+++ frontend/src/views/layout/components/Sidebar/index.vue
@@ -1,18 +1,25 @@
 <template>
-  <scroll-bar>
-    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#fff" active-text-color="#409EFF">
-      <sidebar-item :routes="routes"></sidebar-item>
+  <el-scrollbar wrapClass="scrollbar-wrapper">
+    <el-menu
+      mode="vertical"
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      active-text-color="#409EFF"
+    >
+      <sidebar-item v-for="route in routes" :key="route.name" :item="route" :base-path="route.path"></sidebar-item>
     </el-menu>
-  </scroll-bar>
+  </el-scrollbar>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
-import ScrollBar from '@/components/ScrollBar'
 
 export default {
-  components: { SidebarItem, ScrollBar },
+  components: { SidebarItem },
   computed: {
     ...mapGetters([
       'sidebar'
